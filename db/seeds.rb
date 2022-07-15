f9d9b8ab4b21110fec05f08bd383799a11ca4a23
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-07-15 10:50:04 -0600

    US26: applicant show page working; add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Shelter.destroy_all
+Applicant.destroy_all
+Pet.destroy_all
+VeterinaryOffice.destroy_all
+Veterinarian.destroy_all
+
+shelter_1 = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+shelter_2 = Shelter.create!(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
+shelter_3 = Shelter.create!(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+shelter_4 = Shelter.create!(name: 'Habitat for Animals', city: 'Coulder, CO', foster_program: true, rank: 7)
+
+sally = Applicant.create!(name: 'Sally', address: '123 California St, Boulder, CO, 80304', description: 'I rock!', status: "Pending")
+sally = Applicant.create!(name: 'Joe', address: '321 Jones St, Grand Junction, CO, 87654', description: 'I like dags!', status: "Rejected")
+sally = Applicant.create!(name: 'Peyton', address: '456 Manning St, Fort Collins, CO, 85214', description: 'I rock!', status: "In Progress")
+sally = Applicant.create!(name: 'Betty', address: '654 White St, Denver, CO, 80211', description: 'I rock!', status: "Accepted")
+
+shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+shelter_4.pets.create!(name: 'Pipo', breed: 'ragdoll', age: 2, adoptable: false)
+shelter_4.pets.create!(name: 'Lobster', breed: 'doberman', age: 4, adoptable: true)
+
+vet_office_1 = VeterinaryOffice.create!(name: 'Special Friends', boarding_services: true, max_patient_capacity: 100)
+vet_office_2 = VeterinaryOffice.create!(name: 'Pet Emergency Room', boarding_services: true, max_patient_capacity: 50)
+vet_office_3 = VeterinaryOffice.create!(name: 'The Country Vet', boarding_services: true, max_patient_capacity: 200)
+vet_office_4 = VeterinaryOffice.create!(name: 'Best Vets', boarding_services: true, max_patient_capacity: 20)
+
+vet_office_1.veterinarians.create!(name: 'Morgan', on_call: true, review_rating: 10)
+vet_office_2.veterinarians.create!(name: 'Heather', on_call: true, review_rating: 6)
+vet_office_3.veterinarians.create!(name: 'John', on_call: true, review_rating: 9)
+vet_office_4.veterinarians.create!(name: 'Taylor', on_call: true, review_rating: 7)
+vet_office_2.veterinarians.create!(name: 'Jim', on_call: true, review_rating: 8)
