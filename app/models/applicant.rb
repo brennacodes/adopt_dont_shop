a303b3c6ad61357d757d77ba0db6c367d9589bdb
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-07-15 20:56:57 -0600

    US23: update applicant model

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,5 +1,26 @@
 class Applicant < ApplicationRecord
+  has_many :applicant_pets
   has_many :pets, through: :applicant_pets
 
+  attribute :status, default: "In Progress"
   validates_presence_of :name, :address, :description, :status
+
+  def application_submitted
+    write_attribute(:status, "Pending")
+    save
+  end
+
+  def application_accepted
+    write_attribute(:status, "Accepted")
+    save
+  end
+
+  def application_rejected
+    write_attribute(:status, "Rejected")
+    save
+  end
+
+  def self.current_applicants
+    where("status != ?", "In Progress").or(where("status != ?", "Rejected")).or(where("status != ?", "Accepted"))
+  end
 end
