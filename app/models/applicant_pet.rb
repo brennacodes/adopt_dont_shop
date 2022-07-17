8c5460c547bfe82b24c0db3563dbd1ff0e322626
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-07-17 10:27:55 -0600

    US17: add after_create in applicant_pet model

--- app/models/applicant_pet.rb
+++ app/models/applicant_pet.rb
@@ -2,7 +2,13 @@ class ApplicantPet < ApplicationRecord
   belongs_to :pet
   belongs_to :applicant
 
+  after_create :pet_adopted
+
   def pets_name
     pet.name
+  end
+
+  def pet_adopted
+    pet.update(adoptable: false)
   end
 end
