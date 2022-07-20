7ca9f92669da2e583321165fa64cdda876e27fe7
Bryan Shears <brshears@gmail.com>
2022-07-19 21:14:44 -0600

    Add params status to update method

--- app/controllers/admin_applicants_controller.rb
+++ app/controllers/admin_applicants_controller.rb
@@ -5,15 +5,12 @@ class AdminApplicantsController < AdminsController
 
     def show
       @applicant = Applicant.find(params[:id])
-      # require "pry"; binding.pry
     end
 
     def update
       applicant = Applicant.find(params[:id])
       applicant_pet = ApplicantPet.find_by(pet_id: params[:pet_id], applicant_id: params[:id])
-      # require "pry"; binding.pry
-      applicant_pet.update(status: "Approved")
-      applicant_pet.update(status: "Rejected")
+      applicant_pet.update(status: params[:status])
       redirect_to "/admin/applicants/#{applicant.id}"
     end
 end
